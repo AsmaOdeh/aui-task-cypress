f68a20a6535fc51add595781e7b4d672c11c95f6
Asma <asma.odeh@hp.com>
2021-08-15 01:06:24 +0300

    Edit index.js 2

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,13 +19,8 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-Cypress.on('uncaught:exception', (err, runnable, promise) => {
-    // when the exception originated from an unhandled promise
-    // rejection, the promise is provided as a third argument
-    // you can turn off failing the test in this case
-    if (promise) {
-      return false
-    }
-    // we still want to ensure there are no other unexpected
-    // errors, so we let them fail the test
-  })
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
